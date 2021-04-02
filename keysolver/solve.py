637d0a9d065fb1caa87e0089a2947adb87b4795e
Anthony Strazzella <anthony.strazzella@epita.fr>
2021-04-03 00:17:14 +0200

    add(lib): beginning of solve function

---
+++ keysolver/solve.py
@@ -0,0 +1,10 @@
+from keysolver.io.capture import Capture
+
+def solve(file_path, output_file_path=None):
+    capture = Capture(file_path)
+
+    # TODO: ia process
+
+    # TODO: post treatment
+
+    # TODO: write results
