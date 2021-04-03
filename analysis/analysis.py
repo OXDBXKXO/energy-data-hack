3e4d2d8788ca436a1d832d6511c41fc7ef222f0c
OXDBXKXO <oxdbxkxo@pm.me>
2021-04-03 20:14:53 +0200

    naive mean approach

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import os, math
+
 import plot
 
 
@@ -78,44 +80,132 @@ class Capture:
     def frames(self):
         return self.__frames
 
-class Fingerprint:
+
+class Mean:
     def __init__(self, data):
-        self.nokey = Capture.load_from_file("../../given/data/pics_NOKEY.bin").frames
-        self.file = Capture.load_from_file("../../given/data/{}".format(data)).frames
+        self.frames = Capture.load_from_file(os.path.dirname(__file__) + os.path.sep + ".." + os.path.sep + "given" + os.path.sep + "data" + os.path.sep + "{}".format(data)).frames
 
-        self.diffs = []
-        for i in range(len(self.file)):
-            if i >= len(self.nokey):
-                break
-
-            diff = []
-            exit_for = False
-            for j in range(len(self.nokey[i])):
-                if j >= len(self.file[i]):
-                    exit_for = True
-                    break
-
-                diff.append(self.file[i][j] - self.nokey[i][j])
-
-            if exit_for:
-                break
-
-            self.diffs.append(diff)
-
-    def mean(self):
+    def get(self):
         means = []
-        for diff in self.diffs:
-            means.append(np.mean(diff))
+        for i in range(17):
+            mean = []
+            for frame in self.frames:
+                mean.append(frame[i])
+            means.append(np.mean(mean))
 
-        return np.mean(means)
+        return means
+
+
+def differenceFactor(frames_mean_1, frames_mean_2):
+    factor = 0
+    for i in range(17):
+        factor += math.pow(abs(frames_mean_1[i] - frames_mean_2[i]), 2)
+    return factor
+
+# ['CTRL', '0', 'SUPPR']
+# ['I', 'SHIFT']
+# ['U', 'B', 'H']
+# ['W', 'A', 'Q']
+# ['C']
+# ['K']
+# ['W', 'Q', 'A']
+# ['G', 'T']
+# ['O']
+# ['N']
+# ['1', '3', '2']
+# ['SUPPR', '0']
+# ['4', '3', '1', '2']
+# ['4', '1', '2', '3']
+# ['1', '3', '4', 'ENTER']
+
+def buildWordsTree(likelyLetters):
+
+    if not len(likelyLetters):
+        print("buildWordsTree: Invalid input")
+        return
+
+    special = ["CTRL", "ENTER", "NOKEY", "SHIFT", "SPACE", "SUPPR"]
+
+    # Cleaning list
+    for letters in likelyLetters:
+        for index, letter in enumerate(letters):
+            if letter in special:
+                letters.pop(index)
+
+    previous_stack = []
+    next_stack = []
+
+    for stackFill in likelyLetters[0]:
+        previous_stack.append(stackFill)
+
+    for i in range(1, len(likelyLetters)):
+        likelyLetter = likelyLetters[i]
+
+        while len(previous_stack) != 0:
+            word = previous_stack.pop()
+
+            for letter in likelyLetter:
+                next_stack.append(word + letter)
+        
+        previous_stack = next_stack
+        next_stack = []
+    
+    return previous_stack
+
+
 
 
 def main():
-    pics_pad0 = Fingerprint("pics_1.bin")
-    print(pics_pad0.mean())
-    # pics_nokey = Capture.load_from_file("../../given/data/pics_NOKEY.bin")
-    # pics_pad0 = Capture.load_from_file("../../given/data/pics_0.bin")
+    # pics_pad0 = Fingerprint("pics_1.bin")
+    # pics_pad0.mean()
+    # print(Mean("pics_0.bin").get())
 
+    # return
+
+    test = [['CTRL', '0', 'SUPPR'], ['I', 'SHIFT'], ['U', 'B', 'H'], ['W', 'A', 'Q'], ['C'], ['K'], ['W', 'Q', 'A'], ['G', 'T'], ['O'], ['N'], ['1', '3', '2'], ['SUPPR', '0'], ['4', '3', '1', '2'], ['4', '1', '2', '3'], ['1', '3', '4', 'ENTER']]
+
+    words = buildWordsTree(test)
+
+    f = open("words.txt", "w")
+    for word in words:
+        f.write(word + '\n')
+    f.close()
+
+    return
+
+    keys_means = {}
+    
+    alphanum = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    special = ["CTRL", "ENTER", "NOKEY", "SHIFT", "SPACE", "SUPPR"]
+    
+    for c in alphanum:
+        keys_means[c] = Mean("pics_" + c + ".bin").get()
+    for c in special:
+        keys_means[c] = Mean("pics_" + c + ".bin").get()
+
+    login_frames = Capture.load_from_file(os.path.dirname(__file__) + os.path.sep + ".." + os.path.sep + "given" + os.path.sep + "data" + os.path.sep + "{}".format("pics_LOGINMDP.bin")).frames
+
+    password = []
+    for login_frame in login_frames:
+        lowest_factor = math.inf
+        letter = ""
+        for key in keys_means:
+            factor = differenceFactor(login_frame, keys_means[key])
+            if factor < lowest_factor:
+                lowest_factor = factor
+                letter = key
+
+        likely_match = {}
+        likely_match[letter] = lowest_factor
+        password.append(likely_match)
+    
+    last_letter = ""
+    for letter in password:
+        for l in letter:
+            if l == last_letter:
+                continue
+            print(l)
+            last_letter = l
     # plt.figure(1)
     # # NO KEY
     # for i in range(10):
