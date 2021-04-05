d7cc39ca60f314a7be46cd79e7942ea3f637db69
Anthony Strazzella <anthony.strazzella@epita.fr>
2021-04-05 20:45:03 +0200

    up(lib): finish solve function

--- keysolver/solve.py
+++ keysolver/solve.py
@@ -1,11 +1,34 @@
-from keysolver.io.output_writing import write_data_to_output
-from keysolver.io.capture import Capture
-from keysolver.ai.run import run_ai
+from keysolver.ai import run_ai
+from keysolver.io import write_data_to_output
+from keysolver.post_treatment import get_all_possible_sequences
+from keysolver.post_treatment import filter_keys
 
 def solve(file_path, output_file_path=None):
-    pred = run_ai(file_path)
-    # TODO: ia process
+    """
+    Solve the key sequences contained in the file given as argument.
 
-    # TODO: post treatment
+    Args:
+        file_path:          The path of the file.
+        output_file_path:   The file in which store the result.
 
-    # TODO: write results
+    Returns:
+        A list of possible key sequences.
+
+    Examples:
+        >>> solve("./given/pics_LOGINMDP.bin")
+        ['CTRL SHIFT D SHIFT T SHIFT M SHIFT I H W C K A T O N 2 0 2 2 ENTER',
+        'CTRL SHIFT D SHIFT T SHIFT M SHIFT I H W C K W G O N 3 0 2 4 ENTER'...]
+    """
+
+    # Get the prediction of the ai
+    prediction = run_ai(file_path)
+    # Filter the keys
+    filtered_keys = filter_keys(prediction)
+    # Get the possible sequences
+    possible_sequences = get_all_possible_sequences(filtered_keys)
+
+    # Write sequences in a file if requested
+    if output_file_path != None:
+        write_data_to_output(possible_sequences, output_file_path)
+
+    return possible_sequences
