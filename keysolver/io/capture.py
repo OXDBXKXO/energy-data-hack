22c37144649e852ba5687a1d9bca4f80c4582778
Anthony Strazzella <anthony.strazzella@epita.fr>
2021-04-05 17:18:49 +0200

    add(lib): comments in io files

--- keysolver/io/capture.py
+++ keysolver/io/capture.py
@@ -2,6 +2,21 @@ import numpy as np
 
 
 def read_int(file):
+    """
+    Read an int from a file.
+
+    Args:
+        file:       The file in which read the int.
+
+    Returns:
+        The int read.
+
+    Examples:
+        >>> with open("example", "rb") as file:
+        >>>     read_int(file)
+        42
+    """
+
     ba = bytearray(4)
     file.readinto(ba)
     prm = np.frombuffer(ba, dtype=np.int32)
@@ -10,6 +25,21 @@ def read_int(file):
 
 
 def read_double(file):
+    """
+    Read a double from a file.
+
+    Args:
+        file:       The file in which read the double.
+
+    Returns:
+        The double read.
+
+    Examples:
+        >>> with open("example", "rb") as file:
+        >>>     read_double(file)
+        42
+    """
+
     ba = bytearray(8)
     file.readinto(ba)
     prm = np.frombuffer(ba, dtype=np.double)
@@ -17,7 +47,22 @@ def read_double(file):
     return prm[0]
 
 
-def read_double_tab(file, n):
+def read_double_arr(file, n):
+    """
+    Read a double array from a file.
+
+    Args:
+        file:       The file in which read the double array.
+
+    Returns:
+        The numpy double array read.
+
+    Examples:
+        >>> with open("example", "rb") as file:
+        >>>     read_double_tab(file, 3)
+        numpy.array([42, 42, 42])
+    """
+
     ba = bytearray(8 * n)
     nr = file.readinto(ba)
 
@@ -30,6 +75,17 @@ def read_double_tab(file, n):
 
 
 class Capture:
+    """
+    Capture class.
+
+    Attributes:
+        spikes_per_frame:   The number of spikes per frame.
+        freq_sampling_khz:  The frequence of a sampling (in kHz).
+        freq_frame_hz:      The frequence of a frame (in Hz).
+        freq_spike_khz:     The frequence of a spike (in kHz).
+        norm_fact:          The normalization factor.
+        frames:             List of numpy arrays of the frames.
+    """
 
     def __init__(self, file_path):
         with open(file_path, "rb") as file:
@@ -41,13 +97,16 @@ class Capture:
 
             self.__frames = []
 
-            frame = read_double_tab(file, self.__spikes_per_frame)
+            frame = read_double_arr(file, self.__spikes_per_frame)
 
             while len(frame) != 0:
                 self.__frames.append(frame)
-                frame = read_double_tab(file, self.__spikes_per_frame)
+                frame = read_double_arr(file, self.__spikes_per_frame)
 
     def print_info(self):
+        """
+        Print the info a a Capture object.
+        """
         print(f"Spikes per frames: {self.spikes_per_frame}")
         print(f"Sampling frequence: {self.__freq_sampling_khz} kHz")
         print(f"Frame frequence: {self.__freq_frame_hz} Hz")
@@ -56,12 +115,21 @@ class Capture:
 
     @property
     def spikes_per_frame(self):
+        """
+        Get the number of spikes per frame of a Capture object.
+        """
         return self.__.spikes_per_frame
 
     @property
     def frames(self):
+        """
+        Get the list of the numpy arrays of the frames of a Capture object.
+        """
         return self.__frames
 
     @property
     def nb_frames(self):
+        """
+        Get the number of frames of a Capture object.
+        """
         return len(self.__frames)
--- keysolver/io/output_writing.py
+++ keysolver/io/output_writing.py
@@ -1,8 +1,28 @@
 def write_data_to_output(data, file_path = None):
+    """
+        Write the container given as argument.
+        Each element is printed on a line.
+
+        Args:
+            data:       The data to write. It is treated as a container.
+
+            file_path:  The path of the file in which put the data.
+                        If no file name is given, put the string on stdout.
+
+        Examples:
+            >>> write_data_to_output(["this", "is", "an", "example"])
+            this
+            is
+            an
+            example
+    """
+
+    # Print on stdout
     if file_path is None:
         for k in data:
             print(k)
 
+    # Print in a file
     else:
         with open(file_path, "w") as file:
             for k in data:
